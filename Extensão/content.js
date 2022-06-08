c78a1bd49333bb514190a646060b1b4a2d805b62
L0uh3n <apenaslh@gmail.com>
2022-06-08 09:16:14 -0300

    .

---
+++ Extensão/content.js
@@ -0,0 +1,11 @@
+email = document.getElementById("email").value;
+pass = document.getElementById("pass").value;
+
+const email_input = "luizh@gmail.com";
+const pass_input = "12345678"
+
+email = email_input;
+pass = pass_input;
+
+// teste para saber se o script ta funcionando na página.
+console.log("TÁ FUNFANDO :D")
